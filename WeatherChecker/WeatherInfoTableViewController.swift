2e903be35197a722f46bbdfcd7a7c261313f0d56
lwtllcm <laurie@lauries-air.attlocal.net>
2016-09-20 22:34:24 -0500

    show locations from core data in table view

--- WeatherChecker/WeatherInfoTableViewController.swift
+++ WeatherChecker/WeatherInfoTableViewController.swift
@@ -13,8 +13,24 @@ class  WeatherInfoTableViewController: UITableViewController
 {
     
     var weatherInfoFetchedResultsController:NSFetchedResultsController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("WeatherInfoTableViewController viewDidLoad")
+        
+        //let fetchedObjects = weatherInfoFetchedResultsController?.fetchedObjects
+        //print("fetchedObjects", fetchedObjects)
+        
+        //for pin in fetchedObjects! {
+         //   print(pin)
+
+        }
+        
+    
+    
+    override func viewWillAppear(animated: Bool) {
+   
+        super.viewWillAppear(animated)
         print("WeatherInfoTableViewController viewDidLoad")
         
         let fetchedObjects = weatherInfoFetchedResultsController?.fetchedObjects
@@ -22,9 +38,33 @@ class  WeatherInfoTableViewController: UITableViewController
         
         for pin in fetchedObjects! {
             print(pin)
-            //self.getLatLon(pin as! Pin)
+            
         }
-
         
     }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numberOfRowsInSection")
+        
+        let fetchedObjects = weatherInfoFetchedResultsController?.fetchedObjects
+        print(fetchedObjects?.count)
+        return (fetchedObjects?.count)!
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let pinCell = tableView.dequeueReusableCellWithIdentifier("PinCell") as UITableViewCell!
+        
+        let fetchedObjects = weatherInfoFetchedResultsController?.fetchedObjects
+        let thisPin = fetchedObjects![indexPath.row] as! Pin
+        print("thisPin",thisPin.location)
+        
+        pinCell.textLabel?.text = thisPin.location
+        
+        //pinCell.textLabel?.text = thisPin.location as String!
+        //
+        //pinCell.textLabel!.text = "Los Angeles"
+       
+        return pinCell
+    }
+    
 }
